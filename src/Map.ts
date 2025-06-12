dbc0402244e21d945a6c14f704372150b28a48bd
mdabuzafar <mdabuzafar78@gmail.com>
2025-06-12 14:11:56 +0530

    feat: enhance Map class constructor to accept zoom level through MapOptions interface

--- src/Map.ts
+++ src/Map.ts
@@ -5,14 +5,18 @@ interface Mappable {
   };
 }
 
+interface MapOptions {
+  zoom: number;
+}
+
 export class Map {
   private googleMap: google.maps.Map;
 
-  constructor(divId: string) {
+  constructor(divId: string, options: MapOptions) {
     this.googleMap = new google.maps.Map(
       document.getElementById(divId) as HTMLElement,
       {
-        zoom: 1,
+        zoom: options.zoom,
         zoomControl: true,
         center: {
           lat: 0,
